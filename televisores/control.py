00635006b824716c244323e0ee3aa5e93fac5a95
Uribej2711 <159493293+Uribej2711@users.noreply.github.com>
2024-02-27 00:59:10 -0500

    un nuevo control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,9 +1,35 @@
 class Control:
-    def __init__(self, tv):
-        self._tv = tv
 
     def enlazar(self, tv):
-        self._tv = tv
+        self.setTv(tv)
+        self.__tv.setControl(self)
 
-    def get_tv(self):
-        return self._tv
+    def getTv(self):
+        return self.__tv
+
+    def setTv(self, tv):
+        self.__tv = tv
+
+    def turnOn(self):
+        self.__tv.turnOn()
+
+    def turnOff(self):
+        self.__tv.turnOff()
+
+    def canalUp(self):
+        self.__tv.canalUp()
+
+    def canalDown(self):
+        self.__tv.canalDown()
+
+    def volumenUp(self):
+        self.__tv.volumenUp()
+
+    def volumenDown(self):
+        self.__tv.volumenDown()
+    
+    def setCanal(self, canal):
+        self.__tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        self.__tv.setVolumen(volumen)
